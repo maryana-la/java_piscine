6a4333fb0356a008f62d2f6e5b84b588ce38ee6f
rchelsea <rchelsea@student.42.fr>
2023-03-16 20:13:22 +0100

    ex00

--- day02/ex00/Program.java
+++ day02/ex00/Program.java
@@ -9,59 +9,48 @@ import java.util.*;
 
 public class Program {
 
-    private static final byte[] PNG_HEADER = {
-            (byte) 0x89,
-            (byte) 0x50, (byte) 0x4E, (byte) 0x47,
-            (byte) 0x0D, (byte) 0x0A,
-            (byte) 0x1A,
-            (byte) 0x0A
-    };
-
-    private static final int FILE_HEADER_SIZE = PNG_HEADER.length;
+    private static final int FILE_HEADER_SIZE = 8;
     public static void main (String[] args) {
 
         // open file
         File file = new File("./signatures.txt");
-        Map <String, ArrayList<String>> signature = new HashMap<>();
+        Map<ArrayList<String>, String> signature = new HashMap<>();
         try {
             Scanner sc = new Scanner(file);
             while (sc.hasNextLine()) {
                 String tmp = sc.nextLine();
-                signature.put(getKey(tmp), getValue(tmp));
+                signature.put(getValue(tmp), getKey(tmp));        // save info to map
             }
             System.out.println(signature);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
         }
-        catch (Exception e) {
-        System.out.println(e.getMessage());
-        }
-
-        // save info to map
-
-
-
-
-
 
 
         // check file from stdin
         byte[] buf = new byte[FILE_HEADER_SIZE];
-
-
+        ArrayList<String> fileSign = new ArrayList<>();
         Scanner sc = new Scanner(System.in);
-        try {
-            String fileNameToCheck = sc.nextLine();
-            File fileToCheck = new File(fileNameToCheck);
-            FileInputStream in = new FileInputStream(fileToCheck);
-            System.out.println(in.read(buf));
-            System.out.println(Arrays.toString(buf));
-            for (byte b : buf) {
-                String st = String.format("%02X", b);
-                System.out.print(st + " ");
+        while (sc.hasNextLine()) {
+            try {
+                String fileNameToCheck = sc.nextLine();
+                if (fileNameToCheck.equals("42"))
+                    break;
+                File fileToCheck = new File(fileNameToCheck);
+                FileInputStream in = new FileInputStream(fileToCheck);
+                in.read(buf);
+                for (byte b : buf) {
+                    fileSign.add(String.format("%02X", b));
+                }
+                System.out.println(fileSign);
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
             }
 
-        }
-        catch (Exception e) {
-            System.out.println(e.getMessage());
+            
+
+            // compare our file.Sign among signatures
+            System.out.println(signature.get(fileSign));
         }
     }
 
@@ -73,8 +62,7 @@ public class Program {
 
     private static ArrayList<String> getValue (String line) {
         String[] tmp = line.split(",");
-        String[] tmp1 = tmp[1].split(" ");
 
-        return null;
+        return new ArrayList<String>(Arrays.asList(tmp[1].trim().split("\\s+")));
     }
 }
